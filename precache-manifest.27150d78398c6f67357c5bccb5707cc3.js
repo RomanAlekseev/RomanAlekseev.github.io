ed0aa3f6b36c93883f724510b6f25ebddc1eb1d0
RomanAlekseev <r.alekseev@andersenlab.com>
2019-08-12 18:41:31 +0300

    Add ScrollUp button

---
+++ precache-manifest.27150d78398c6f67357c5bccb5707cc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a617521673f8165fe8d82a2ecef612",
+    "url": "/index.html"
+  },
+  {
+    "revision": "343f8b0a9642a300bd19",
+    "url": "/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "b71fa72529f1850216a2",
+    "url": "/static/css/main.c6a34d78.chunk.css"
+  },
+  {
+    "revision": "343f8b0a9642a300bd19",
+    "url": "/static/js/2.29091403.chunk.js"
+  },
+  {
+    "revision": "b71fa72529f1850216a2",
+    "url": "/static/js/main.55536c36.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
