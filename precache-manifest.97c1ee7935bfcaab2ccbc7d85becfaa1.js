23130845cd77c9b99cbb3a4e5b80ed669ab3bd61
RomanAlekseev <43404726+RomanAlekseev@users.noreply.github.com>
2019-08-12 17:38:00 +0300

    Add files via upload

---
+++ precache-manifest.97c1ee7935bfcaab2ccbc7d85becfaa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2697091477b17c0b0848d5919a356cdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ab9c4d421559bc44223",
+    "url": "/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "183ca1ef2a1a0a598770",
+    "url": "/static/css/main.c6a34d78.chunk.css"
+  },
+  {
+    "revision": "7ab9c4d421559bc44223",
+    "url": "/static/js/2.39e42d6c.chunk.js"
+  },
+  {
+    "revision": "183ca1ef2a1a0a598770",
+    "url": "/static/js/main.21f74141.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
